7b6d9183c2f890b454f44896b0ade8e1dc22a98c
Matiss Kiploks <matiss-toms.kiploks@edu.rtu.lv>
2024-12-13 04:59:45 +0200

    file: remove debug text
    Signed-off-by: Matiss Kiploks <matiss-toms.kiploks@edu.rtu.lv>

--- file.c
+++ file.c
@@ -35,32 +35,24 @@ struct student * data_read( const char *file, int *count ){
 	*count = 1;
 	struct student *data = NULL;
 
-	printf("Count is %d\n", *count);
-	//return NULL;
-
 	if (!file )
 		return data;
-	else
-		printf("Data file is %s\n", file);
-
 	fd = open( file, O_RDWR, 0600 );
 	if ( fd < 0 ){
 		printf("Error opening file: %d ($m)\n", fd);
 		return data;
-	}else
-		printf("File description is %d", fd);
+	}
 
 	data = (struct student *)malloc( sizeof(struct student) );
 	if ( !data )
-		return data;
+		goto exit;
 
 	while( 1 ) {
 		br = read( fd, data + *count, sizeof(struct student) );
-		if ( br < sizeof(struct student) )
+		if ( br != sizeof(struct student) )
 			break;
 
 		(*count)++;
-		printf("Count is %d\n", *count);
 		data = (struct student *)realloc( data, sizeof(struct student) * (*count + 1) );
 		if ( !data )
 			break;
