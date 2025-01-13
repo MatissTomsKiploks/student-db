da9343afaef1158c0c6d61bc2728ada8bbf5cc16
Matiss Kiploks <matiss-toms.kiploks@edu.rtu.lv>
2025-01-13 16:02:16 +0200

    file, main, inout, common: rewrote all of the code
    Signed-off-by: Matiss Kiploks <matiss-toms.kiploks@edu.rtu.lv>

--- common.h
+++ common.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+/*
 #define MAX_STUDENTS	10
 
 enum {
@@ -24,3 +24,21 @@ struct student{
 void data_input( int count, struct student *s );
 void data_output( int count, struct student *s );
 int data_save( int count, struct student *s, const char *file);
+*/
+
+#define MAX_STUDENTS	10
+
+enum {
+    DATA_SAVE_OK,
+    DATA_SAVE_ERR_INPUT,
+    DATA_SAVE_ERR_OPEN,
+    DATA_SAVE_ERR_WRITE,
+    DATA_READ_ERR_FILE
+};
+
+struct student {
+    char name[20];
+    char lastname[20];
+    int group_id;
+    int course;
+};
